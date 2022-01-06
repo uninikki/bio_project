89dbfbbec9afbe98f6665fc03fbfc6e20e24e901
uninikki <nikki.smith20001@gmail.com>
2022-01-06 01:28:53 -0500

    Convert to amino acid complete

--- BioApplication.py
+++ BioApplication.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+import textwrap
 from tkinter import *
 # BioApplication class code from https://www.youtube.com/watch?v=jBUpjijYtCk
 
@@ -8,14 +9,14 @@ class BioApplication(tk.Tk):
         tk.Tk.__init__(self, *args, **kwargs)
         container = tk.Frame(self)
 
-        container.pack(side="top", fill="both", expand = True)
+        container.pack(side="top", fill="both", expand=True)
 
         container.grid_rowconfigure(0, weight=1)
         container.grid_columnconfigure(0, weight=1)
 
         self.frames = {}
 
-        for F in (DNAtoRNA, DNACompliment):
+        for F in (DNAtoRNA, DNACompliment, RNAtoAminoAcid):
             frame = F(container, self)
             self.frames[F] = frame
             print(self.frames)
@@ -27,6 +28,8 @@ class BioApplication(tk.Tk):
         frame = self.frames[cont]
         frame.tkraise()
 
+
+# todo Create an abstract frame class
 
 class DNAtoRNA(tk.Frame):
     def __init__(self, parent, controller):
@@ -56,11 +59,11 @@ class DNAtoRNA(tk.Frame):
         self.button.pack()
 
         self.rna_to_aa = tk.Button(self, text='RNA to amino acid', font=("Power green", 12),
-                                   command=lambda : controller.show_frame(RNAtoAminoAcid))
+                                   command=lambda: controller.show_frame(RNAtoAminoAcid))
         self.rna_to_aa.pack(fill=X, expand=TRUE, side=LEFT)
 
         self.dna_compliment = tk.Button(self, text='DNA Compliment Finder', font=("Power green", 12),
-                                        command=lambda : controller.show_frame(DNACompliment))
+                                        command=lambda: controller.show_frame(DNACompliment))
         self.dna_compliment.pack(fill=X, expand=TRUE, side=LEFT)
 
     def button_clicked(self):
@@ -100,11 +103,12 @@ class DNACompliment(tk.Frame):
         self.button['command'] = self.button_clicked
         self.button.pack()
 
-        self.rna_to_aa = tk.Button(self, text='RNA to Amino Acid', font=("Power green", 12))
+        self.rna_to_aa = tk.Button(self, text='RNA to Amino Acid', font=("Power green", 12),
+                                   command=lambda: controller.show_frame(RNAtoAminoAcid))
         self.rna_to_aa.pack(fill=X, expand=TRUE, side=LEFT)
 
         self.dna_to_rna = tk.Button(self, text='DNA to RNA', font=("Power green", 12),
-                                    command=lambda : controller.show_frame(DNAtoRNA))
+                                    command=lambda: controller.show_frame(DNAtoRNA))
         self.dna_to_rna.pack(fill=X, expand=TRUE, side=LEFT)
 
     def button_clicked(self):
@@ -130,15 +134,61 @@ class RNAtoAminoAcid(tk.Frame):
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
 
+        # Root window
+        # self.title("DNA -> RNA")
+        # self.geometry("500x200")
+
+        # labels
+        self.title = tk.Label(self, text='RNA -> Amino Acid', font=("Power green", 14))
+        self.title.pack(pady=10, padx=10)
+
+        self.label = tk.Label(self, text='Insert RNA strand here!', font=("Power green", 14))
+        self.label.pack()
+
+        # entries
+        self.entry = Entry(self, width=100)
+        self.entry.pack()
+
+        self.entry_result = Entry(self, width=100, state="disabled")
+        self.entry_result.pack()
+
+        # buttons
+        self.button = tk.Button(self, text='Convert to Amino acid/s', font=("Power green", 12))
+        self.button['command'] = self.button_clicked
+        self.button.pack()
+
+        self.rna_to_aa = tk.Button(self, text='Visualize DNA', font=("Power green", 12),
+                                   command=lambda: controller.show_frame(VisualizeDNA))
+        self.rna_to_aa.pack(fill=X, expand=TRUE, side=LEFT)
+
+        self.dna_compliment = tk.Button(self, text='DNA Compliment Finder', font=("Power green", 12),
+                                        command=lambda: controller.show_frame(DNACompliment))
+        self.dna_compliment.pack(fill=X, expand=TRUE, side=LEFT)
+
+    def button_clicked(self):
+        # enable the entry box before anything is done
+        self.entry_result.config(state="normal")
+        # Clear this textbox before inserting anything else
+        self.entry_result.delete(0, "end")
+        text = self.entry.get()
+        valid_text = is_valid_rna(text)
+
+        # Check which button was clicked, and behave accordingly
+        if not valid_text:
+            self.entry_result.insert(0, "Invalid RNA strand entered.")
+        else:
+            # call functions that divides the rna into triplets, converts to amino acids,
+            # and insert that into the result entry box.
+            self.entry_result.insert(0, convert_to_aa(divide_rna(text)))
+            # divides the original entry into triplets for readability
+            self.entry.delete(0, "end")
+            self.entry.insert(0, divide_rna(text))
+            # disable the resulting text box.
+        self.entry_result.config(state="disabled")
+
 
 # Todo
 class VisualizeDNA(tk.Frame):
-    def __init__(self, parent, controller):
-        tk.Frame.__init__(self, parent)
-
-
-# TODO
-class VisualizeRNA(tk.Frame):
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
 
@@ -184,6 +234,49 @@ def convert_to_rna(template: str) -> str:
         else:
             rna_compliment += "A"
     return rna_compliment
+
+
+def is_valid_rna(rna: str) -> bool:
+    text = rna.upper()
+    check = True
+    for nucleotide in text:
+        if nucleotide.isnumeric() or not nucleotide.isalpha():
+            check = False
+        elif nucleotide != "A" and nucleotide != "U" and nucleotide != "C" and nucleotide != "G":
+            check = False
+    return check
+
+
+def divide_rna(rna: str) -> str:
+    wrapper = textwrap.wrap(rna, 3)
+    divided = ""
+    for triplet in wrapper:
+        if wrapper.index(triplet) != 0:
+            divided += " " + triplet
+        else:
+            divided += triplet
+    return divided
+
+
+def convert_to_aa(rna: str) -> str:
+    divided_rna = divide_rna(rna).split(" ")
+    aa = ""
+    codons = {"Ala": ["GCA", "GCC", "GCG", "GCU"], "Cys": ["UGC", "UGU"], "Asp": ["GAC", "GAU"],
+              "Ile": ["AUU", "AUC", "AUA"], "Leu": ["CUU", "CUC", "CUA", "CUG", "UUA", "UUG"],
+              "Val": ["GUU", "GUC", "GUA", "GUG"], "Phe": ["UUU", 'UUC'], "Met": ["AUG"],
+              "Gly": ["GGU", "GGC", "GGA", "GGG"], "Pro": ["CCU", "CCC", "CCA", "CCG"],
+              "Thr": ["ACU", "ACC", "ACA", "ACG"], "Ser": ["UCU", "UCC", "UCA", "UCG", "AGU", "AGC"],
+              "Tyr": ["UAU", "UAC"], "Trp": ["UGG"], "Gln": ["CAA", "CAG"], "Asn": ["AAU", "AAC"],
+              "His": ["CAU", "CAC"], "Glu": ["GAA", "GAG"], "Lys": ["AAA", "AAG"],
+              "Arg": ["CGU", "CGC", "CGA", "CGG", "AGA", "AGG"], "Stop": ["UAA", "UAG", "UGA"]}
+
+    for triplet in divided_rna:
+        for key, value in codons.items():
+            if triplet in value and aa == "":
+                aa += key
+            elif triplet in value and aa != "":
+                aa += " " + key
+    return aa
 
 
 if __name__ == "__main__":
